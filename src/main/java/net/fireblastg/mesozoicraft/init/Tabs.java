9162073c936b8f3b51831e150b7c1b2b16a21f25
FireBlastGaming <iboo.abdulali@gmail.com>
2023-05-12 19:57:22 +0400

    Renamed Classes for convenience.

--- src/main/java/net/fireblastg/mesozoicraft/init/Tabs.java
+++ src/main/java/net/fireblastg/mesozoicraft/init/Tabs.java
@@ -1,9 +1,10 @@
-package net.fireblastg.mesozoicraft.item;
+package net.fireblastg.mesozoicraft.init;
 
+import net.fireblastg.mesozoicraft.init.item.ItemInit;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
-public class ModCreativeModeTab {
+public class Tabs {
     public static final CreativeModeTab MESOZOICRAFT_TAB = new CreativeModeTab("mesozoicraft_tab") {
         @Override
         public ItemStack makeIcon() {
