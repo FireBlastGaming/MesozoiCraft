3ba5fe821db94b96e215c83f7f0f45cd04437d78
FireBlastGaming <iboo.abdulali@gmail.com>
2024-11-18 22:04:55 -0700

    Reboot Sequence Initiated: Target Genesis
    Rebooting....
    BEEP
    BOOP
    Loading Genesis files....
    Welcome to MesozoiCraft: Genesis

---
+++ src/main/java/io/github/fireblastgaming/mesozoicraft/MesozoiCraft.java
@@ -0,0 +1,8 @@
+package io.github.fireblastgaming.mesozoicraft;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(MesozoiCraft.MODID)
+public class MesozoiCraft {
+    public static final String MODID = "mesozoicraft";
+}
